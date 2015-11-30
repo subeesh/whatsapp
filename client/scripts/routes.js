dae4cb8906c99fcf878321f6cd280ae4dec0c57f
subeesh <subeesh.a@gmail.com>
2015-11-30 21:15:51 +0800

    angular ionic init + static data

---
+++ client/scripts/routes.js
@@ -0,0 +1,23 @@
+angular
+  .module('Whatsapp')
+  .config(config);
+ 
+function config($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('tab', {
+      url: '/tab',
+      abstract: true,
+      templateUrl: 'client/templates/tabs.html'
+    })
+    .state('tab.chats', {
+      url: '/chats',
+      views: {
+        'tab-chats': {
+          templateUrl: 'client/templates/chats.html',
+          controller: 'ChatsCtrl'
+        }
+      }
+    });
+ 
+  $urlRouterProvider.otherwise('tab/chats');
+}
