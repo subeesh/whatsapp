0bdaf415d0192ca8d6a901023e64b129b1801f8b
subeesh <subeesh.a@gmail.com>
2015-11-30 22:25:23 +0800

    send new message

--- client/scripts/controllers/chat-detail.controller.js
+++ client/scripts/controllers/chat-detail.controller.js
@@ -2,11 +2,53 @@ angular
   .module('Whatsapp')
   .controller('ChatDetailCtrl', ChatDetailCtrl);
  
-function ChatDetailCtrl ($scope, $stateParams) {
+function ChatDetailCtrl ($scope, $stateParams, $ionicScrollDelegate, $timeout, $meteor) {
   var chatId = $stateParams.chatId;
+  var isIOS = ionic.Platform.isWebView() && ionic.Platform.isIOS();
   $scope.chat = $scope.$meteorObject(Chats, chatId, false);
 
   $scope.messages = $scope.$meteorCollection(function () {
     return Messages.find({ chatId: chatId });
   }, false);
+
+  $scope.data = {};
+  $scope.sendMessage = sendMessage;
+  $scope.inputUp = inputUp;
+  $scope.inputDown = inputDown;
+  $scope.closeKeyboard = closeKeyboard;
+ 
+  function sendMessage () {
+	if (_.isEmpty($scope.data.message)) {
+      return;
+    }
+ 
+    $meteor.call('newMessage', {
+      text: $scope.data.message,
+      chatId: chatId
+    });
+ 
+    delete $scope.data.message;  	
+  }
+
+  function inputUp () {
+    if (isIOS) {
+      $scope.data.keyboardHeight = 216;
+    }
+ 
+    $timeout(function() {
+      $ionicScrollDelegate.$getByHandle('chatScroll').scrollBottom(true);
+    }, 300);
+  }
+
+  function inputDown () {
+    if (isIOS) {
+      $scope.data.keyboardHeight = 0;
+    }
+ 
+    $ionicScrollDelegate.$getByHandle('chatScroll').resize();
+  }
+ 
+  function closeKeyboard () {
+    // cordova.plugins.Keyboard.close();
+  }
 }
